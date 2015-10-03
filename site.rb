b5bed2033c98a57589246b5026c11bacd27e044d
Nat Welch <nat@natwelch.com>
2015-10-03 19:37:10 +0000

    copy over stuff from rsd

--- site.rb
+++ site.rb
@@ -11,6 +11,11 @@ configure do
     :production => ENV['DATABASE_URL']
   }
 
+  if !RACK_ENV.eql? :development
+    # Force HTTPS
+    use Rack::SslEnforcer
+  end
+
   url = URI(connections[RACK_ENV])
   options = {
     :adapter => url.scheme,
@@ -40,16 +45,14 @@ configure do
   end
 end
 
-get "/" do
-  if session[:uid]
-    @user = User.where(id: session[:uid]).first
-    @batches = access_token_get(session[:token], "https://recurse.com/api/v1/batches")
+# God I love filters!
+before do
+  # pass the filter if still in auth phase
+  pass if /^\/(auth\/|logout).*/.match(request.path_info)
 
-    if @user.nil?
-      error 500
-    else
-      erb :index
-    end
+  # setup current_user if the user is logged in
+  if session[:uid]
+    @current_user = User.where(id: session[:uid]).first
   else
     redirect "/auth/recurse_center"
   end
@@ -58,34 +61,14 @@ end
 %w(get post).each do |method|
   send(method, "/auth/:provider/callback") do
     # https://github.com/intridea/omniauth/wiki/Auth-Hash-Schema
-    # #<OmniAuth::AuthHash
-    # batch=#<OmniAuth::AuthHash end_date="2015-07-02" id=18 name="Spring 2, 2015" start_date="2015-03-30">
-    # batch_id=18
-    # bio="I'm originally from San Francisco (and the surrounding area), but I've spent the last year or so living in London. I've previously worked at Google, iFixit, Adode and Punchd. \r\n\r\nBeyond that, I'm a contributor to Fog (a ruby library for managing the cloud), and spend a lot of time thinking about managing distributed systems and deploying code. I like consuming and creating \"Art\" (painting, photography, music, books, old, new, street, whatever) and enjoy a good game of D&D.\r\n\r\nI love a good cup of coffee or pint of beer, and a nice long walk to find either.\r\n\r\nI'm currently unemployed and considering jobs in New York, Seattle and the Bay Area."
-    # email="nat@natwelch.com"
-    # first_name="Nat"
-    # github="icco"
-    # has_photo=true
-    # id=1150
-    # image="https://d29xw0ra2h4o4u.cloudfront.net/assets/people/nat_welch_150-6e067d098ed9324e3627df919f656a89.jpg"
-    # is_faculty=false
-    # is_hacker_schooler=true
-    # job=nil
-    # last_name="Welch"
-    # links=[#<OmniAuth::AuthHash title="natwelch.com" url="http://natwelch.com">]
-    # middle_name=nil
-    # phone_number="+17077998675"
-    # projects=[#<OmniAuth::AuthHash description="My blog for interesting links and daily posts about RC. Hand coded in go. https://github.com/icco/natnatnat" title="Nat? Nat. Nat!" url="https://writing.natwelch.com/">, #<OmniAuth::AuthHash description="Hyperspace is a multiplayer version of Asteroids. https://github.com/kenpratt/hyperspace" title="Hyperspace" url="http://playhyperspace.com/">, #<OmniAuth::AuthHash description="A p2p audio streaming app built entirely in javascript for hosting silent dance parties. https://github.com/pselle/shhparty" title="ShhParty" url="http://shhparty.herokuapp.com/">, #<OmniAuth::AuthHash description="A ruby site for tracking how much I work on personal projects. https://github.com/icco/code.natwelch.com" title="Nat's Code" url="http://code.natwelch.com/">]
-    # skills=["java", "ruby", "Linux", "git", "php", "golang", "distributed systems", "google cloud", "python"]
-    # twitter="icco">
+    session[:uid] = env["omniauth.auth"]["uid"]
+    session[:token] = env["omniauth.auth"]["credentials"]["token"]
 
-    session[:uid] = env['omniauth.auth']['uid']
-    session[:token] = env['omniauth.auth']['credentials']['token']
 
     u = User.find_or_create_by(id: session[:uid])
-    u.name = env['omniauth.auth']['info']['name']
-    u.image = env['omniauth.auth']['info']['image']
-    u.email = env['omniauth.auth']['info']['email']
+    u.name = env["omniauth.auth"]["info"]["name"]
+    u.image = env["omniauth.auth"]["info"]["image"]
+    u.batch = env["omniauth.auth"]["info"]["batch"]["name"]
     u.save
 
     redirect "/"
@@ -100,8 +83,4 @@ end
 error 400..510 do
   @code = response.status
   erb :error
-end
-
-def access_token_get token, target
-
 end
